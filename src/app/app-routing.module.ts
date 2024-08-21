a30f3ef868a5c86ae86ed337a98050795b45a7fd
Siddhesh Rane <ruckerrane@gmail.com>
2024-08-20 23:59:24 -0400

    Updated core app component to display home component.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { OrderInformationComponent } from './order-information/order-information.component';
+import { ResolverServiceService } from './services/resolver-service.service';
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  {path: '', redirectTo: 'home',pathMatch: 'full', resolve:{product : ResolverServiceService}},
+  {path: 'home',component: HomeComponent, resolve:{product : ResolverServiceService}},
+  {path: 'orders',component: OrderInformationComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
+  providers:[ResolverServiceService],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,46 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatCardModule }from '@angular/material/card'
+import {MatTabsModule} from '@angular/material/tabs'
+import { HttpClientModule } from '@angular/common/http';
+import { PlaceOrderComponent } from './place-order/place-order.component';
+import { MatButtonModule } from '@angular/material/button';
+import { OrderInformationComponent } from './order-information/order-information.component';
+import { MatDialogModule } from '@angular/material/dialog';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ModalComponent } from './shared/modal/modal.component';
+import {MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+
+ 
+
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PlaceOrderComponent,
+    OrderInformationComponent,
+    ModalComponent,
+       
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatTabsModule,
+    HttpClientModule,
+    MatCardModule,
+    MatButtonModule,
+    NgbModule,
+    MatProgressSpinnerModule
   ],
+  exports: [MatDialogModule],
   providers: [],
   bootstrap: [AppComponent]
 })
